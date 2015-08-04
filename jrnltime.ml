4b40d01a074cadf04ac74a13acb7695206407b7c
Erik Lovlie <eriklovlie@gmail.com>
2015-08-04 21:00:17 +0200

    Added ocaml version for fun.
    Currently only prints the hours in lieu.

--- jrnltime.ml
+++ jrnltime.ml
@@ -5,9 +5,21 @@ let main =
   |> Jrnljson_j.jrnl_of_string
   |> fun jrnl ->
      let open Jrnljson_t in
-     List.iter jrnl.entries
-               ~f:(fun entry -> printf "entry: %s\n" (entry.title ^ " " ^ entry.body))
-     
+     let n_hours =
+       let workstart = List.filter jrnl.entries ~f:(fun e -> String.is_substring ~substring:"workstart" e.title)
+       and workend = List.filter jrnl.entries ~f:(fun e -> String.is_substring ~substring:"workend" e.title)
+       and workhours e1 e2 =
+         let t1 = Time.of_string (e1.date ^ "T" ^ e1.time)
+         and t2 = Time.of_string (e2.date ^ "T" ^ e2.time) in
+         Time.Span.to_hr (Time.diff t2 t1)
+       in
+       let hours = List.map2_exn workstart workend ~f:workhours in
+       List.fold hours ~init:0.0 ~f:(+.)
+     in
+     let n_days = List.fold jrnl.entries ~init:String.Set.empty ~f:(fun a e -> Set.add a e.date) |> Set.length in
+     let in_lieu = n_hours -. Float.of_int (n_days * 8) in
+     printf "days: %d, hours in lieu: %f\n" n_days in_lieu
+
 let spec =
   let open Command.Spec in
   empty
@@ -16,7 +28,7 @@ let spec =
 let command =
   Command.basic
     ~summary:"Time tracking using jrnl"
-    ~readme:(fun () -> "Bla bla")
+    ~readme:(fun () -> "Extract hours in lieu from your jrnl.")
     spec
     (fun () -> main)
 
